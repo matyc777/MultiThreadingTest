a2a41c2e9e00cea40bfb4b260c428ea39050f150
matyc <matyc777@gmail.com>
2018-04-17 23:19:25 +0300

    no message

---
+++ MultiThreadingTest/Charting.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Windows.Forms;
+
+namespace MultiThreadingTest
+{
+    public partial class Charting : Form
+    {
+        int ConstComplexity;
+        int ConstThreads;
+        int Size;
+        MultiThreadingTest test;
+
+        public Charting()
+        {
+            InitializeComponent();
+        }
+
+        private void Charting_Load(object sender, EventArgs e)
+        {
+            textBox3.Text = "0";
+            textBox2.Enabled = true;
+            textBox3.Enabled = false;
+            chart1.Series[0].ChartType = SeriesChartType.Spline;
+            chart1.Series[0].BorderWidth = 3;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.Clear();
+            Size = int.Parse(textBox1.Text);
+            try
+            {
+                ConstComplexity = int.Parse(textBox2.Text);
+                ConstThreads = int.Parse(textBox3.Text);
+                if (ConstThreads > Size) throw new Exception();
+                test = new MultiThreadingTest(Size, ConstComplexity, ConstThreads);
+                if (radioButton1.Checked)
+                {
+                    chart1.Series[0].Points.AddXY(1, test.MediumTime(1));
+                    chart1.Series[0].Points.AddXY(2, test.MediumTime(2));
+                    chart1.Series[0].Points.AddXY(10, test.MediumTime(10));
+                    chart1.Series[0].Points.AddXY(20, test.MediumTime(20));
+                    chart1.Series[0].Points.AddXY(Size / 2, test.MediumTime(Size / 2));
+                    chart1.Series[0].Points.AddXY(Size, test.MediumTime(Size));
+                }
+                else
+                {
+                    chart1.Series[0].Points.AddXY(1000, test.MediumTimeConstThreads(1000));
+                    chart1.Series[0].Points.AddXY(2000, test.MediumTimeConstThreads(2000));
+                    chart1.Series[0].Points.AddXY(3000, test.MediumTimeConstThreads(3000));
+                    chart1.Series[0].Points.AddXY(4000, test.MediumTimeConstThreads(4000));
+                    chart1.Series[0].Points.AddXY(5000, test.MediumTimeConstThreads(5000));
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Wrong input");
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                label4.Text = "x - Threads, y - Time";
+                textBox3.Text = "0";
+                textBox2.Text = "";
+                textBox2.Enabled = true;
+                textBox3.Enabled = false;
+            }
+            else
+            {
+                label4.Text = "x - Complexity, y - Time";
+                textBox3.Text = "";
+                textBox2.Text = "0";
+                textBox2.Enabled = false;
+                textBox3.Enabled = true;
+            }
+        }
+    }
+}
---
+++ MultiThreadingTest/MultiThreadingTest.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Diagnostics;
+
+namespace MultiThreadingTest
+{
+    class MultiThreadingTest
+    {
+        int Size;
+        int Complexity;
+        int Threads;
+        double[] a;
+        double[] b;
+
+        public MultiThreadingTest(int Size, int Complexity, int Threads)
+        {
+            this.Threads = Threads;
+            this.Size = Size;
+            this.Complexity = Complexity;
+            a = new double[Size];
+            b = new double[Size];
+            Random rnd = new Random();
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = rnd.Next(1000, 2000);
+            }
+        }
+
+        void Input(object BeginEnd)
+        {
+            string[] str = BeginEnd as string[];
+            for (int i = int.Parse(str[0]); i < int.Parse(str[1]); i++)
+            {
+                for (int j = 0; j < Complexity; j++) b[i] += Math.Pow(a[i], 2);//1.789
+            }
+        }
+
+        void StartThreads(int Number)
+        {
+            int begin = 0;
+            int end = Size / Number;
+            string[] str = new string[2];
+            List<Thread> threads = new List<Thread>();
+
+            for (int i = 0; i < Number; i++)
+            {
+                threads.Add(new Thread(new ParameterizedThreadStart(Input)));
+            }
+
+            foreach (Thread thrd in threads)
+            {
+                str[0] = string.Format("{0}", begin);
+                str[1] = string.Format("{0}", end);
+                thrd.Start(str as object);
+                begin += Size / Number;
+                end += Size / Number;
+                Thread.Sleep(10);
+            }
+
+            foreach (Thread thrd in threads)
+            {
+                thrd.Join();
+            }
+            Array.Clear(b, 0, Size);
+        }
+
+        public int MediumTime(int NumberOfThreads)
+        {
+            int AllTimeMiliseconds = 0;
+            for (int n = 0; n < 10; n++)
+            {
+                Stopwatch stopWatch = new Stopwatch();
+                stopWatch.Start();
+                StartThreads(NumberOfThreads);
+                stopWatch.Stop();
+                TimeSpan ts = stopWatch.Elapsed;
+                AllTimeMiliseconds += ts.Milliseconds;
+            }
+            return AllTimeMiliseconds / 10;
+        }
+
+        public int MediumTimeConstThreads(int Complexity)
+        {
+            this.Complexity = Complexity;
+            int AllTimeMiliseconds = 0;
+            for (int n = 0; n < 10; n++)
+            {
+                Stopwatch stopWatch = new Stopwatch();
+                stopWatch.Start();
+                StartThreads(Threads);
+                stopWatch.Stop();
+                TimeSpan ts = stopWatch.Elapsed;
+                AllTimeMiliseconds += ts.Milliseconds;
+            }
+            return AllTimeMiliseconds / 10;
+        }
+    }
+}
